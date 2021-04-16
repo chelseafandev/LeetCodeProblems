0114fd5be380dd39d6b57732056e67ad775969c5
junhaeng90 <bluetomorrow90@gmail.com>
2021-04-17 03:26:24 +0900

    ReverseInteger 문제풀이 추가(2)

--- Easy/ReverseInteger.cpp
+++ Easy/ReverseInteger.cpp
@@ -1,9 +1,62 @@
 #include <iostream>
+#include <cmath>
 
 class Solution
 {
 public:
     int reverse(int x)
     {
+        int nRet = 0;
+        bool isNegative = x < 0 ? true : false;
+
+        if (isNegative && x < (-1 * (pow(2, 31) - 1)))
+            return 0;
+        else if (isNegative && x >= (-1 * (pow(2, 31) - 1)))
+            x *= -1;
+
+        std::string toStr = std::to_string(x);
+        std::string reverseStr;
+        std::string::reverse_iterator ritr;
+        for (ritr = toStr.rbegin(); ritr != toStr.rend(); ritr++)
+        {
+            reverseStr.push_back(*ritr);
+        }
+
+        if (isNegative)
+        {
+            // 음수
+            if (-1 * pow(2, 31) > -1 * std::stod(reverseStr))
+            {
+                std::cout << "(Negative) Out of bound" << std::endl;
+                return 0;
+            }
+            else
+            {
+                nRet = -1 * std::stod(reverseStr);
+            }
+        }
+        else
+        {
+            // 양수
+            if (pow(2, 31) - 1 < std::stod(reverseStr))
+            {
+                std::cout << "(Postive) Out of bound" << std::endl;
+                return 0;
+            }
+            else
+            {
+                nRet = std::stod(reverseStr);
+            }
+        }
+
+        return nRet;
     }
 };
+
+int main()
+{
+    Solution s;
+    std::cout << s.reverse(-2147483648) << std::endl;
+    std::cout << s.reverse(-2147483647) << std::endl;
+    return 0;
+}

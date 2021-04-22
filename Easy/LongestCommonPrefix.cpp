30f5196e6b0d08bc1748fde39c2ecde391f29d97
junhaeng90 <bluetomorrow90@gmail.com>
2021-04-22 18:22:22 +0900

    1일1문 Easy LongestCommonPrefix 문제 추가

---
+++ Easy/LongestCommonPrefix.cpp
@@ -0,0 +1,39 @@
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lower-case English letters.
+*/
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    std::string longestCommonPrefix(std::vector<std::string>& strs) {
+        
+    }
+};
+
+int main() 
+{
+    Solution s;
+
+    return 0;
+}

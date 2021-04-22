129014b377eceea9dcfc6fc809ea71f1c4a7ebf7
junhaeng90 <bluetomorrow90@gmail.com>
2021-04-22 19:01:21 +0900

    1일1문 Easy LongestCommonPrefix 문제 풀이 추가

--- Easy/LongestCommonPrefix.cpp
+++ Easy/LongestCommonPrefix.cpp
@@ -24,16 +24,79 @@ strs[i] consists of only lower-case English letters.
 #include <iostream>
 #include <vector>
 
-class Solution {
+class Solution
+{
 public:
-    std::string longestCommonPrefix(std::vector<std::string>& strs) {
+    std::string longestCommonPrefix(std::vector<std::string> &strs)
+    {
+        int inputLen = strs.size();
+        std::vector<char> vecPrefix;
+        std::vector<char> vecCandidate;
+        std::vector<std::string>::iterator itr;
+        int idx = 0;
+        while (1)
+        {
+            for (itr = strs.begin(); itr != strs.end(); itr++)
+            {
+                if((*itr).length() == 0)
+                {
+                    break;
+                }
+
+                // idx 체크
+                if (idx > (*itr).length() - 1)
+                {
+                    break;
+                }
+
+                if (vecCandidate.empty())
+                {
+                    // tmp가 비어있는 경우
+                    vecCandidate.push_back((*itr).at(idx));
+                }
+                else
+                {
+                    if (vecCandidate.at(0) == (*itr).at(idx))
+                    {
+                        vecCandidate.push_back((*itr).at(idx));
+                    }
+                }
+            }
+
+            if (vecCandidate.size() == inputLen)
+            {
+                vecPrefix.push_back(vecCandidate.at(0));
+                vecCandidate.clear();
+                idx++;
+            }
+            else
+            {
+                break;
+            }
+        }
         
+        if(vecPrefix.size() == 0)
+        {
+            return "";
+        }
+        else
+        {
+            std::string retStr = "";
+            std::vector<char>::iterator itr;
+            for(itr = vecPrefix.begin(); itr != vecPrefix.end(); itr++)
+            {
+                retStr += (*itr);
+            }
+
+            return retStr;
+        }
     }
 };
 
-int main() 
+int main()
 {
+    std::vector<std::string> strs = {"flower","flow","flight"};
     Solution s;
-
+    std::cout << s.longestCommonPrefix(strs) << std::endl;
     return 0;
 }

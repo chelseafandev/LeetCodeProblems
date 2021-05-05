0b37306f56832628ef60c5ea67b6b5c48aac831c
junhaeng90 <bluetomorrow90@gmail.com>
2021-05-06 04:53:24 +0900

    1일 1문 Medium ZigZag Convention 문제 풀이 추가

--- Medium/ZigZagConversion.cpp
+++ Medium/ZigZagConversion.cpp
@@ -40,17 +40,98 @@ s consists of English letters (lower-case and upper-case), ',' and '.'.
 1 <= numRows <= 1000
 */
 #include <iostream>
+#include <vector>
+
+#define MAX_COLUMN_SIZE 1000
 
 class Solution
 {
 public:
     std::string convert(std::string s, int numRows)
     {
-        //
+        char **plate = nullptr;
+        plate = new char *[numRows];
+        for (int i = 0; i < numRows; i++)
+        {
+            plate[i] = new char[MAX_COLUMN_SIZE];
+        }
+
+        // 공백으로 초기화(나중에 공백인 경우에는 결과 값에서 제외하기 위해)
+        for (int i = 0; i < numRows; i++)
+        {
+            for (int j = 0; j < MAX_COLUMN_SIZE; j++)
+            {
+                plate[i][j] = ' ';
+            }
+        }
+
+        int nLen = s.length();
+        int nCnt = 0;
+        int nCol = 0;
+
+        while (nCnt <= nLen)
+        {
+            // phase1
+            for (int i = 0; i < numRows; i++)
+            {
+                plate[i][nCol] = s.at(nCnt);
+                //printf("plate[%d][%d] = %c\n", i, nCol, plate[i][nCol]);
+                nCnt++;
+                if (nCnt >= nLen)
+                {
+                    return makeResult(plate, numRows);
+                }
+            }
+
+            if (numRows > 1)
+            {
+                // phsae2
+                for (int i = 0; i < numRows - 2; i++)
+                {
+                    nCol++;
+                    int nRow = numRows - 2 - i;
+                    plate[nRow][nCol] = s.at(nCnt);
+                    //printf("plate[%d][%d] = %c\n", nRow, nCol, plate[nRow][nCol]);
+                    nCnt++;
+                    if (nCnt >= nLen)
+                    {
+                        return makeResult(plate, numRows);
+                    }
+                }
+                nCol++;
+            }
+            else
+            {
+                nCol++;
+            }
+        }
+
+        return makeResult(plate, numRows);
+    }
+
+private:
+    std::string makeResult(char **plate, int numRows)
+    {
+        std::string strRet = "";
+        for (int i = 0; i < numRows; i++)
+        {
+            for (int j = 0; j < MAX_COLUMN_SIZE; j++)
+            {
+                if (plate[i][j] != ' ')
+                {
+                    strRet += plate[i][j];
+                }
+            }
+        }
+        return strRet;
     }
 };
 
 int main()
 {
+    std::string input = "PAYPALISHIRING";
+    //std::string input = "AB";
+    Solution s;
+    std::cout << s.convert(input, 1) << std::endl;
     return 0;
 }

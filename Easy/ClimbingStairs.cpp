8cfa54078e35b64e6b8b79810a4a3113aa1a320e
junhaeng90 <bluetomorrow90@gmail.com>
2021-05-21 18:36:13 +0900

    1일 1문 Easy ClimbingStairs 문제 풀이 추가

--- Easy/ClimbingStairs.cpp
+++ Easy/ClimbingStairs.cpp
@@ -26,17 +26,28 @@ Constraints:
 1 <= n <= 45
 */
 #include <iostream>
+#include <vector>
 
 class Solution {
 public:
     int climbStairs(int n) {
         
+        // 점화식 만들기 문제인가..
+        std::vector<int> v;
+        v.push_back(1);
+        v.push_back(2);
+        for(int i = 2; i < n; i++)
+        {
+            v.push_back(v[i-1] + v[i-2]);
+        }
+
+        return v[n-1];
     }
 };
 
 int main()
 {
-    int input = 2;
+    int input = 5;
     Solution s;
     std::cout << s.climbStairs(input) << std::endl;
     return 0;

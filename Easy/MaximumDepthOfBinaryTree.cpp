a55fb906d7ff2e93b53662b16517b2a72219c77d
junhaeng90 <bluetomorrow90@gmail.com>
2021-06-29 23:57:48 +0900

    1일 1문 Easy Maximum Depth Of Binary Tree 문제 추가

---
+++ Easy/MaximumDepthOfBinaryTree.cpp
@@ -0,0 +1,70 @@
+/*
+Given the root of a binary tree, return its maximum depth.
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+
+Example 2:
+Input: root = [1,null,2]
+Output: 2
+
+Example 3:
+Input: root = []
+Output: 0
+
+Example 4:
+Input: root = [0]
+Output: 1
+ 
+
+Constraints:
+The number of nodes in the tree is in the range [0, 10^4].
+-100 <= Node.val <= 100
+*/
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    int maxDepth(TreeNode *root)
+    {
+        int left_max = 0;
+        if (root != nullptr)
+        {
+            left_max = subTreeDepth(root->left, 0);
+            subTreeDepth(root->right);
+        }
+    }
+
+private:
+    int subTreeDepth(TreeNode *root, int count)
+    {
+        if (root != nullptr)
+        {
+            subTreeDepth(root->left, count++);
+            subTreeDepth(root->right, count++);
+        }
+        else
+        {
+            return count;
+        }
+    }
+};
+
+int main()
+{
+    Solution s;
+
+    return 0;
+}

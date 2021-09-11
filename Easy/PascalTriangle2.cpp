f3506ec8e19cc192c0675f87f3374428435c53d2
junhaeng90 <bluetomorrow90@gmail.com>
2021-09-11 23:27:32 +0900

    1일 1문 Easy Pascal's Triangle 2 문제 추가. 최적화 신경쓰기

---
+++ Easy/PascalTriangle2.cpp
@@ -0,0 +1,42 @@
+/*
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+Example 1:
+Input: rowIndex = 3
+Output: [1,3,3,1]
+
+Example 2:
+Input: rowIndex = 0
+Output: [1]
+
+Example 3:
+Input: rowIndex = 1
+Output: [1,1]
+ 
+
+Constraints:
+0 <= rowIndex <= 33
+
+Follow up: Could you optimize your algorithm to use only O(rowIndex) extra space?
+*/
+
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+    std::vector<int> getRow(int rowIndex)
+    {
+        // 최적화에 신경써보자(extra space를 O(rowIndex)만 사용해서)
+        
+        
+    }
+};
+
+int main()
+{
+    return 0;
+}

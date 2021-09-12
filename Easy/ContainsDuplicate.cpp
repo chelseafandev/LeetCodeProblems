b1808585a716477de0f3514ba89f437cf2b58d58
junhaeng90 <bluetomorrow90@gmail.com>
2021-09-12 17:07:26 +0900

    1일 1문 Easy Cotains Duplicate 문제 풀이 추가

---
+++ Easy/ContainsDuplicate.cpp
@@ -0,0 +1,51 @@
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example 1:
+Input: nums = [1,2,3,1]
+Output: true
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: false
+
+Example 3:
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+ 
+
+Constraints:
+1 <= nums.length <= 10^5
+-10^9 <= nums[i] <= 10^9
+*/
+
+#include <iostream>
+#include <vector>
+#include <set>
+
+class Solution
+{
+public:
+    bool containsDuplicate(std::vector<int> &nums)
+    {
+        std::set<int> s;
+
+        std::vector<int>::iterator itr;
+        for(itr = nums.begin(); itr != nums.end(); itr++)
+        {
+            auto result = s.insert(*itr);
+            if(!result.second)
+                return true;
+        }
+        
+        return false;
+    }
+};
+
+int main()
+{
+    std::vector<int> input = {1,1,1,3,3,4,3,2,4,2};
+    Solution s;    
+    std::cout << std::boolalpha << s.containsDuplicate(input) << std::endl;
+    return 0;
+}

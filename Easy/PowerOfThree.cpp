3c35335d14ff7232e1827e9cfe722f2acd27b51e
junhaeng90 <bluetomorrow90@gmail.com>
2021-10-01 18:39:10 +0900

    Power of Three 문제 수정

--- Easy/PowerOfThree.cpp
+++ Easy/PowerOfThree.cpp
@@ -1,6 +1,6 @@
 /*
 Given an integer n, return true if it is a power of three. Otherwise, return false.
-An integer n is a power of three, if there exists an integer x such that n == 3x.
+An integer n is a power of three, if there exists an integer x such that n == 3^x.
 
 Example 1:
 Input: n = 27

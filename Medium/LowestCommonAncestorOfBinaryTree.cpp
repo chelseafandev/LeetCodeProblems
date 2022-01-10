7b5951970d882b27cb8166b55e79ac662e033670
chelseafandev <chelseafandev@gmail.com>
2022-01-10 17:47:28 +0900

    algo: 1w 1a 2022년 1월 첫째주 문제(Lowest Common Ancestor of a Binary Tree) 풀이 추가
    - complete binary tree인 경우에만 트리를 배열로 변환하여 배열의 인덱스 활용이 가능하니 주의

--- Medium/LowestCommonAncestorOfBinaryTree.cpp
+++ Medium/LowestCommonAncestorOfBinaryTree.cpp
@@ -25,24 +25,89 @@ p and q will exist in the tree.
 */
 
 #include <iostream>
+#include <deque>
 
-struct TreeNode
-{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+#include "Common/Helper.h"
 
 class Solution
 {
 public:
     TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
     {
+        p_val_ = p->val;
+        q_val_ = q->val;
+
+        std::deque<int> path;
+        find_path(root, path);
+
+        // 전략
+        // p와 q 노드에 도달하는 path 정보(from root to node p, q)를 저장해두고 양쪽 path에서 중복되는 노드 정보를 찾는다. 중복되는 노드들 중 가장 마지막 노드가 LCA다!
+        // 노드 p까지 도달하는 path를 기준으로 노드 q까지 도달하는 path를 비교한다.
+        int lca_val = 0;
+        while (!p_path_.empty())
+        {
+            // q is empty
+            if (q_path_.empty())
+            {
+                break;
+            }
+
+            // not equal
+            if (p_path_.front() != q_path_.front())
+            {
+                break;
+            }
+
+            // equal
+            lca_val = p_path_.front(); // update lca value
+            p_path_.pop_front();
+            q_path_.pop_front();
+        }
+
+        return new TreeNode(lca_val);
     }
+
+private:
+    // root에서부터 노드 p, q까지 도달하는 path를 구하는 함수
+    // 재귀 호출을 활용하여 path를 tracking한다
+    void find_path(TreeNode *root, std::deque<int> &path)
+    {
+        if (root == nullptr)
+        {
+            return;
+        }
+
+        path.push_back(root->val);
+        if (root->val == p_val_)
+        {
+            p_path_ = path;
+        }
+        else if (root->val == q_val_)
+        {
+            q_path_ = path;
+        }
+
+        find_path(root->left, path);
+        find_path(root->right, path);
+        path.pop_back();
+    }
+
+    int p_val_;
+    std::deque<int> p_path_;
+
+    int q_val_;
+    std::deque<int> q_path_;
 };
 
 int main()
 {
+    // std::string input = "3,5,1,6,2,0,8,null,null,7,4";
+    // std::string input = "-1,0,3,-2,4,null,null,8";
+    std::string input = "37,-34,-48,null,-100,-101,48,null,null,null,null,-54,null,-71,-22,null,null,null,8";
+    util::Helper h;
+    TreeNode *root = h.convertStr2Tree(input);
+    Solution s;
+    auto result = s.lowestCommonAncestor(root, new TreeNode(-71), new TreeNode(8));
+    std::cout << "find LCA: " << result->val << std::endl;
     return 0;
 }

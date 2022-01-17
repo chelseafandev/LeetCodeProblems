84f9420ebd9f97d607ec82862d58f8f185785f59
chelseafandev <chelseafandev@gmail.com>
2022-01-17 16:25:40 +0900

    algo: 1w 1a 2022.01 2nd week 문제 풀이 추가 - find_target_index 함수의 실제 연산 횟수를 찍어보았을때는 유의미한 결과 도출됨 - Leetcode에서 O(logn) 알고리즘임을 검증할 수 있는 Input을 제공해주었으면 더 좋았을듯

--- Medium/FindFirstandLastPositionofElementinSortedArray.cpp
+++ Medium/FindFirstandLastPositionofElementinSortedArray.cpp
@@ -27,16 +27,150 @@ nums is a non-decreasing array.
 
 class Solution {
 public:
+    Solution()
+    {
+        range_left_ = 0;
+        range_right_ = 0;
+
+        /*
+        // for debugging
+        secound_operation_count_ = 0;
+        third_operation_count_ = 0;
+        */
+    }
+
     std::vector<int> searchRange(std::vector<int>& nums, int target)
     {
-        
+        // 해당 문제는 target을 찾고 끝나는것이 아니기 때문에 단순 이진 탐색으로는 target의 전체 범위를 찾을 수 없음. O(logn)을 만족하기 위해서는 어떻게 접근해야할까...?
+        std::vector<int> result;
+
+        // 1. 최초 target을 찾는다. O(logn)
+        bool is_found = false;
+        int start = 0;
+        int end = nums.size() - 1;
+        int mid = 0;
+        while(start <= end)
+        {
+            mid = (start + end) / 2;
+            if (nums[mid] > target)
+            {
+                end = mid - 1;
+            }
+            else if (nums[mid] < target)
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                is_found = true;
+                break;
+            }
+        }
+
+        if (!is_found)
+        {
+            result.push_back(-1);
+            result.push_back(-1);
+            return result;
+        }
+
+        range_left_ = mid;
+        range_right_ = mid;
+
+        // 2. 최초 target 인덱스를 기준으로 양쪽으로 나누어 range의 양끝을 구한다.
+        result.push_back(find_target_index(nums, target, start, mid - 1, true)); // O(logn)
+        // std::cout << "second step operation count: " << secound_operation_count_ << std::endl; // for debugging
+
+        result.push_back(find_target_index(nums, target, mid + 1, end, false)); // O(logn)
+        // std::cout << "third step operation count: " << third_operation_count_ << std::endl; // for debugging
+
+        return result;
     }
+
+private:
+    int find_target_index(const std::vector<int>& nums, int target, int start, int end, bool is_left)
+    {
+        // base condition
+        if (start > end)
+        {
+            if (is_left)
+            {
+                return range_left_;
+            }
+            else
+            {
+                return range_right_;
+            }
+        }
+
+        /*
+        // for debugging
+        if(is_left)
+        {
+            secound_operation_count_++;
+        }
+        else
+        {
+            third_operation_count_++;
+        }
+        */
+
+        int mid = (start + end) / 2;
+        if (nums[mid] > target)
+        {
+            end = mid - 1;
+            return find_target_index(nums, target, start, end, is_left);
+        }
+        else if (nums[mid] < target)
+        {
+            start = mid + 1;
+            return find_target_index(nums, target, start, end, is_left);
+        }
+        else
+        {
+            if (is_left)
+            {
+                // 현재 target을 찾은 인덱스 값인 mid 바로 이전(mid-1)에 중복된 target값이 있다면 range 확장을 위해 탐색을 한번 더 수행한다.
+                if (mid - 1 >= 0 && nums[mid-1] == target)
+                {
+                    end = mid - 1;
+                    return find_target_index(nums, target, start, end, is_left);
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+            else
+            {
+                // 현재 target을 찾은 인덱스 값인 mid 바로 이후(mid+1)에 중복된 target값이 있다면 range 확장을 위해 탐색을 한번 더 수행한다.
+                if (mid + 1 < nums.size() && nums[mid+1] == target)
+                {
+                    start = mid + 1;
+                    return find_target_index(nums, target, start, end, is_left);
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+        }
+    }
+
+    int range_left_;
+    int range_right_;
+
+    /*
+    // for debugging
+    int secound_operation_count_;
+    int third_operation_count_;
+    */
 };
 
 int main()
 {
     std::vector<int> nums = {5,7,7,8,8,10};
-    int target = 9;
+    int target = 8;
     Solution s;
     std::cout << "[ ";
     for (const auto& data : s.searchRange(nums, target))

8c0f68cecc67f45550eeef1f1131b3dd4b90861e
chelseafandev <chelseafandev@gmail.com>
2022-01-27 00:53:45 +0900

    algo: 1w 1a 22년 1월 셋째주 문제 풀이 Time Limit(Word Break) - 특정 인풋에서 무한루프에 빠지는 현상있어서 해결 요망

--- Medium/WordBreak.cpp
+++ Medium/WordBreak.cpp
@@ -42,7 +42,7 @@ public:
     int start_idx_;
     int end_idx_;
     std::string word_;
-}
+};
 
 class Solution
 {
@@ -61,13 +61,13 @@ public:
 
         // 최초 match 단어 찾은 후 stack에 push
         bool is_matched = false;
-        std::string first = "";
+        std::string assembled = "";
         int start_idx = 0;
         int end_idx = 0;
         for (const auto& character : s)
         {
-            first += character;
-            if (words.find(first) != words.end())
+            assembled += character;
+            if (words.find(assembled) != words.end())
             {
                 is_matched = true;
                 break;
@@ -80,23 +80,75 @@ public:
             return false;
         }
 
-        WordInfo info(first, start_idx, end_idx);
+        WordInfo info(assembled, start_idx, end_idx);
         match_index.push(info);
+        
+        int tmp_start_idx = end_idx;
+        int tmp_end_idx = end_idx;
+        assembled = "";
 
         // assemble looping
         // - start_idx와 end_idx는 계속 tracking
-        // - first를 찾은 후 남은 문자열 s에서 wordDict에 매칭되는 단어를 찾았으면 stack에 push
-        // - 찾지 못한 경우라면 stack의 top값(WordInfo)에서 start_idx 및 end_idx를 조회하여 이어서 assemble
+        // - assembled를 찾은 후 남은 문자열 s에서 wordDict에 매칭되는 단어를 찾았으면 stack에 push
+        // - 찾지 못한 경우라면
+        //   : end_idx를 증가시켜 매칭할 단어에 문자를 추가
+        //   : 만약 end_idx가 입력 문자열의 끝에 다달았을 경우, stack의 top값(WordInfo)에서 start_idx 및 end_idx를 조회하여 이어서 assemble
         // - loop 탈출 조건은 다시 한번 생각해보기
+        bool result = true;
+        while( match_index.empty() || (match_index.top().end_idx_ != s.size() - 1) )
+        {   
+            tmp_end_idx++;
+            if(tmp_end_idx >= s.size())
+            {
+                result = false;
+                break;
+            }
 
-        return true;
+            assembled += s[tmp_end_idx];
+            std::cout << "debug >> assemble: \"" << assembled << "\"" << std::endl;
+            if (words.find(assembled) != words.end())
+            {
+                // match!
+                WordInfo info(assembled, tmp_start_idx, tmp_end_idx);
+                match_index.push(info);
+                std::cout << "  debug >> push: \"" << assembled << "\"" << std::endl;
+                assembled = "";
+                tmp_start_idx = tmp_end_idx;
+            }
+            else
+            {
+                // no match!
+                if(tmp_end_idx == s.size() - 1)
+                {
+                    if(match_index.empty())
+                    {
+                        result = false;
+                        break;
+                    }
+
+                    // 문자열 s의 끝에 도달한 경우
+                    auto popped = match_index.top();
+                    match_index.pop();
+                    std::cout << "    debug >> pop: \"" << popped.word_ << "\"" << std::endl;
+
+                    assembled = popped.word_;
+                    tmp_start_idx = popped.start_idx_;
+                    tmp_end_idx = popped.end_idx_;
+                }
+            }
+        }
+
+        return result;
     }
 };
 
 int main()
 {
-    std::string input = "leetcode";
-    std::vector<std::string> wordDict = {"leet", "code"};
+    // std::string input = "fohhemkkaecojceoaejkkoedkofhmohkcjmkggcmnami";
+    // std::vector<std::string> wordDict = {"kfomka","hecagbngambii","anobmnikj","c","nnkmfelneemfgcl","ah","bgomgohl","lcbjbg","ebjfoiddndih","hjknoamjbfhckb","eioldlijmmla","nbekmcnakif","fgahmihodolmhbi","gnjfe","hk","b","jbfgm","ecojceoaejkkoed","cemodhmbcmgl","j","gdcnjj","kolaijoicbc","liibjjcini","lmbenj","eklingemgdjncaa","m","hkh","fblb","fk","nnfkfanaga","eldjml","iejn","gbmjfdooeeko","jafogijka","ngnfggojmhclkjd","bfagnfclg","imkeobcdidiifbm","ogeo","gicjog","cjnibenelm","ogoloc","edciifkaff","kbeeg","nebn","jdd","aeojhclmdn","dilbhl","dkk","bgmck","ohgkefkadonafg","labem","fheoglj","gkcanacfjfhogjc","eglkcddd","lelelihakeh","hhjijfiodfi","enehbibnhfjd","gkm","ggj","ag","hhhjogk","lllicdhihn","goakjjnk","lhbn","fhheedadamlnedh","bin","cl","ggjljjjf","fdcdaobhlhgj","nijlf","i","gaemagobjfc","dg","g","jhlelodgeekj","hcimohlni","fdoiohikhacgb","k","doiaigclm","bdfaoncbhfkdbjd","f","jaikbciac","cjgadmfoodmba","molokllh","gfkngeebnggo","lahd","n","ehfngoc","lejfcee","kofhmoh","cgda","de","kljnicikjeh","edomdbibhif","jehdkgmmofihdi","hifcjkloebel","gcghgbemjege","kobhhefbbb","aaikgaolhllhlm","akg","kmmikgkhnn","dnamfhaf","mjhj","ifadcgmgjaa","acnjehgkflgkd","bjj","maihjn","ojakklhl","ign","jhd","kndkhbebgh","amljjfeahcdlfdg","fnboolobch","gcclgcoaojc","kfokbbkllmcd","fec","dljma","noa","cfjie","fohhemkka","bfaldajf","nbk","kmbnjoalnhki","ccieabbnlhbjmj","nmacelialookal","hdlefnbmgklo","bfbblofk","doohocnadd","klmed","e","hkkcmbljlojkghm","jjiadlgf","ogadjhambjikce","bglghjndlk","gackokkbhj","oofohdogb","leiolllnjj","edekdnibja","gjhglilocif","ccfnfjalchc","gl","ihee","cfgccdmecem","mdmcdgjelhgk","laboglchdhbk","ajmiim","cebhalkngloae","hgohednmkahdi","ddiecjnkmgbbei","ajaengmcdlbk","kgg","ndchkjdn","heklaamafiomea","ehg","imelcifnhkae","hcgadilb","elndjcodnhcc","nkjd","gjnfkogkjeobo","eolega","lm","jddfkfbbbhia","cddmfeckheeo","bfnmaalmjdb","fbcg","ko","mojfj","kk","bbljjnnikdhg","l","calbc","mkekn","ejlhdk","hkebdiebecf","emhelbbda","mlba","ckjmih","odfacclfl","lgfjjbgookmnoe","begnkogf","gakojeblk","bfflcmdko","cfdclljcg","ho","fo","acmi","oemknmffgcio","mlkhk","kfhkndmdojhidg","ckfcibmnikn","dgoecamdliaeeoa","ocealkbbec","kbmmihb","ncikad","hi","nccjbnldneijc","hgiccigeehmdl","dlfmjhmioa","kmff","gfhkd","okiamg","ekdbamm","fc","neg","cfmo","ccgahikbbl","khhoc","elbg","cbghbacjbfm","jkagbmfgemjfg","ijceidhhajmja","imibemhdg","ja","idkfd","ndogdkjjkf","fhic","ooajkki","fdnjhh","ba","jdlnidngkfffbmi","jddjfnnjoidcnm","kghljjikbacd","idllbbn","d","mgkajbnjedeiee","fbllleanknmoomb","lom","kofjmmjm","mcdlbglonin","gcnboanh","fggii","fdkbmic","bbiln","cdjcjhonjgiagkb","kooenbeoongcle","cecnlfbaanckdkj","fejlmog","fanekdneoaammb","maojbcegdamn","bcmanmjdeabdo","amloj","adgoej","jh","fhf","cogdljlgek","o","joeiajlioggj","oncal","lbgg","elainnbffk","hbdi","femcanllndoh","ke","hmib","nagfahhljh","ibifdlfeechcbal","knec","oegfcghlgalcnno","abiefmjldmln","mlfglgni","jkofhjeb","ifjbneblfldjel","nahhcimkjhjgb","cdgkbn","nnklfbeecgedie","gmllmjbodhgllc","hogollongjo","fmoinacebll","fkngbganmh","jgdblmhlmfij","fkkdjknahamcfb","aieakdokibj","hddlcdiailhd","iajhmg","jenocgo","embdib","dghbmljjogka","bahcggjgmlf","fb","jldkcfom","mfi","kdkke","odhbl","jin","kcjmkggcmnami","kofig","bid","ohnohi","fcbojdgoaoa","dj","ifkbmbod","dhdedohlghk","nmkeakohicfdjf","ahbifnnoaldgbj","egldeibiinoac","iehfhjjjmil","bmeimi","ombngooicknel","lfdkngobmik","ifjcjkfnmgjcnmi","fmf","aoeaa","an","ffgddcjblehhggo","hijfdcchdilcl","hacbaamkhblnkk","najefebghcbkjfl","hcnnlogjfmmjcma","njgcogemlnohl","ihejh","ej","ofn","ggcklj","omah","hg","obk","giig","cklna","lihaiollfnem","ionlnlhjckf","cfdlijnmgjoebl","dloehimen","acggkacahfhkdne","iecd","gn","odgbnalk","ahfhcd","dghlag","bchfe","dldblmnbifnmlo","cffhbijal","dbddifnojfibha","mhh","cjjol","fed","bhcnf","ciiibbedklnnk","ikniooicmm","ejf","ammeennkcdgbjco","jmhmd","cek","bjbhcmda","kfjmhbf","chjmmnea","ifccifn","naedmco","iohchafbega","kjejfhbco","anlhhhhg"};
+    
+    std::string input = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
+    std::vector<std::string> wordDict = {"a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"};
     Solution s;
     std::cout << std::boolalpha << s.wordBreak(input, wordDict) << std::endl;
     return 0;

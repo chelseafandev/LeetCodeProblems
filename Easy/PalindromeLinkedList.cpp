9ffffada90cc1ee782c3010002ecfdcafe179fa7
chelseafandev <chelseafandev@gmail.com>
2022-02-04 17:52:44 +0900

    algo: 1w 1a Palindrome Linked List의 Follow up 문제 풀이 추가 - fast_ptr slow_ptr 개념 도입

--- Easy/PalindromeLinkedList.cpp
+++ Easy/PalindromeLinkedList.cpp
@@ -67,9 +67,114 @@ public:
     }
 
     // Follow up: Could you do it in O(n) time and O(1) space?
-    bool isPalindrome_version2(ListNode *head)
+    bool isPalindrome_version2(ListNode* head)
     {
-        
+        ListNode* slow_ptr = head;
+        ListNode* fast_ptr = head;
+
+        ListNode* second_half = head;
+        ListNode* prev_of_slow_ptr = head;
+
+        // To handle odd size list
+        ListNode* midnode = nullptr;
+
+        // initialize result
+        bool res = true;
+
+        if (head != nullptr && head->next != nullptr)
+        {
+            // Get the middle of the list. Move slow_ptr by 1 and fast_ptr by 2, slow_ptr will have the middle node
+            while (fast_ptr != nullptr && fast_ptr->next != nullptr)
+            {
+                fast_ptr = fast_ptr->next->next;
+
+                // We need previous of the slow_ptr for linked lists with odd elements
+                prev_of_slow_ptr = slow_ptr;
+                slow_ptr = slow_ptr->next;
+            }
+
+            // fast_ptr would become nullptr when there are even elements in list. And not nullptr for odd elements.
+            // We need to skip the middle node for odd case and store it somewhere so that we can restore the original list
+            if (fast_ptr != nullptr)
+            {
+                midnode = slow_ptr;
+                slow_ptr = slow_ptr->next;
+            }
+
+            // Now reverse the second half and compare it with first half
+            second_half = slow_ptr;
+
+            // nullptr terminate first half
+            prev_of_slow_ptr->next = nullptr;
+
+            // Reverse the second half
+            reverse(&second_half);
+
+            // compare
+            res = compareLists(head, second_half);
+
+            // Construct the original list back
+            reverse(&second_half); // Reverse the second half again
+
+            // If there was a mid node (odd size case) which was not part of either first half or second half.
+            if (midnode != nullptr)
+            {
+                prev_of_slow_ptr->next = midnode;
+                midnode->next = second_half;
+            }
+            else
+            {
+                prev_of_slow_ptr->next = second_half;
+            }
+        }
+
+        return res;
+    }
+
+private:
+    // Function to reverse the linked list
+    // Note that this function may change the head
+    void reverse(ListNode **head_ref)
+    {
+        ListNode* prev = nullptr;
+        ListNode* current = *head_ref;
+        ListNode* next;
+
+        while (current != nullptr)
+        {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+        *head_ref = prev;
+    }
+
+    // Function to check if two input lists have same data
+    bool compareLists(ListNode* head1, ListNode* head2)
+    {
+        ListNode* temp1 = head1;
+        ListNode* temp2 = head2;
+
+        while (temp1 && temp2)
+        {
+            if (temp1->val == temp2->val)
+            {
+                temp1 = temp1->next;
+                temp2 = temp2->next;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        // Both are empty return 1
+        if (temp1 == nullptr && temp2 == nullptr)
+            return 1;
+
+        // Will reach here when one is nullptr and other is not
+        return 0;
     }
 };
 

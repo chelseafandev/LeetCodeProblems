b04ce7e9291d1a8dce1dcd35fb83536edc15867e
chelseafandev <chelseafandev@gmail.com>
2022-02-07 15:23:44 +0900

    algo: 1w 1a 2022.02 1st 문제 풀이 추가 - LeetCode 117번 Populating Next Right Pointers in Each Node II

--- Medium/PopulatingNextRightPointersinEachNodeII.cpp
+++ Medium/PopulatingNextRightPointersinEachNodeII.cpp
@@ -31,33 +31,63 @@ The recursive approach is fine. You may assume implicit stack space does not cou
 */
 
 #include <iostream>
+#include <vector>
+#include <map>
 
-// Definition for a Node.
-class Node
-{
-public:
-    int val;
-    Node *left;
-    Node *right;
-    Node *next;
-
-    Node() : val(0), left(nullptr), right(nullptr), next(nullptr) {}
-
-    Node(int _val) : val(_val), left(nullptr), right(nullptr), next(nullptr) {}
-
-    Node(int _val, Node *_left, Node *_right, Node *_next)
-        : val(_val), left(_left), right(_right), next(_next) {}
-};
+#include "Common/Helper.h"
 
 class Solution
 {
 public:
-    Node *connect(Node *root)
+    typedef std::map<int, std::vector<SpecialNode*>> map_type;
+
+    SpecialNode* connect(SpecialNode* root)
     {
+        map_type nodes_per_level;
+        make_node_vector(nodes_per_level, root, 0);
+
+        for (const auto& nodes : nodes_per_level)
+        {
+            for (int i = 0; i < nodes.second.size() - 1; i++)
+            {
+                (nodes.second)[i]->next = (nodes.second)[i+1];
+            }
+        }
+
+        return root;
+    }
+
+private:
+    void make_node_vector(map_type& nodes_per_level, SpecialNode* root, int level)
+    {
+        if (root == nullptr)
+        {
+            return;
+        }
+
+        if (nodes_per_level.find(level) != nodes_per_level.end())
+        {
+            nodes_per_level[level].push_back(root);
+        }
+        else
+        {
+            std::vector<SpecialNode*> tmp;
+            tmp.push_back(root);
+            nodes_per_level.insert(std::make_pair(level, tmp));
+        }
+
+        level++;
+        make_node_vector(nodes_per_level, root->left, level);
+        make_node_vector(nodes_per_level, root->right, level);
     }
 };
 
 int main()
 {
+    std::string input = "1,2,3,4,5,null,7";
+    util::Helper h;
+    SpecialNode* root = h.convert_string_to_tree_for_special_node(input);
+    Solution s;
+    s.connect(root);
     return 0;
 }

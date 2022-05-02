582487cde399caf04a954f67f3a2c2841db791c4
chelseafandev <chelseafandev@gmail.com>
2022-05-02 17:38:28 +0900

    algo: leetcode 1e 1m 문제 추가 - 2108. Find First Palindromic String in the Array - 75. Sort Colors

---
+++ Medium/SortColors.cpp
@@ -0,0 +1,47 @@
+/*
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+You must solve this problem without using the library's sort function.
+
+
+Example 1:
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+Example 2:
+Input: nums = [2,0,1]
+Output: [0,1,2]
+
+Constraints:
+n == nums.length
+1 <= n <= 300
+nums[i] is either 0, 1, or 2.
+*/
+
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+    void sortColors(std::vector<int> &nums)
+    {
+    }
+};
+
+int main()
+{
+    std::vector<int> input = {2,0,2,1,1,0};
+
+    Solution s;
+    s.sortColors(input);
+
+    std::cout << "[ ";
+    for (const auto num : input)
+    {
+        std::cout << num << " ";
+    }
+    std::cout << "]" << std::endl;
+
+    return 0;
+}

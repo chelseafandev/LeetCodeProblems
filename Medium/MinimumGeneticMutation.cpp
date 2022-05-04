e28d7f306a96626c5b239bcebd9a9069318994bd
chelseafandev <chelseafandev@gmail.com>
2022-05-04 18:05:33 +0900

    algo: leetcode #433(0ms, 6.6MB) - 433. Minimum Genetic Mutation - 문제 이해하는데 한참 걸림... BFS활용

--- Medium/MinimumGeneticMutation.cpp
+++ Medium/MinimumGeneticMutation.cpp
@@ -31,20 +31,72 @@ start, end, and bank[i] consist of only the characters ['A', 'C', 'G', 'T'].
 
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <set>
 
 class Solution
 {
 public:
     int minMutation(std::string start, std::string end, std::vector<std::string> &bank)
     {
+        // start가 end가 되기위해 필요한 최소한의 mutation의 횟수를 구하는것이 문제 (`1 mutation` is `one single character changed in the gene string`)
+        std::vector<char> candidate = {'A', 'C', 'G', 'T'};
+        std::set<std::string> dict(bank.begin(), bank.end());
+
+        if (dict.find(end) == dict.end())
+        {
+            return -1;
+        }
+            
+        int result = 0;
+        std::queue<std::string> q;
+        q.push(start);
+
+        while (!q.empty())
+        {
+            int n = q.size();
+            for (int i = 0; i < n; i++)
+            {
+                std::string popped = q.front();
+                q.pop();
+
+                if (popped == end)
+                {
+                    return result;
+                }
+                    
+                dict.erase(popped);
+
+                // popped된 8자리의 gene string에서 1개의 문자만 바뀌는 케이스들을 모두 구해서 유효한 mutation이 있는지를 dict에서 조회
+                for (int j = 0; j < 8; j++)
+                {
+                    char c = popped[j];
+                    for (int k = 0; k < 4; k++)
+                    {
+                        popped[j] = candidate[k];
+                        if (dict.find(popped) != dict.end())
+                        {
+                            q.push(popped);
+                            std::cout << "push: " << popped << std::endl;
+                        }
+                    }
+                    popped[j] = c; // 원복
+                }
+            }
+
+            result++;
+            std::cout << "result: " << result << std::endl;
+        }
+
+        return -1;
     }
 };
 
 int main()
 {
-    std::string start = "AACCGGTT";
-    std::string end = "AACCGGTA";
-    std::vector<std::string> bank = {"AACCGGTA"};
+    std::string start = "AAAAACCC";
+    std::string end = "AACCCCCC";
+    std::vector<std::string> bank = {"AAAACCCC","AAACCCCC","AACCCCCC"};
     
     Solution s;
     std::cout << s.minMutation(start, end, bank) << std::endl;
